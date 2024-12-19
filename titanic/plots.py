e5cd1f714a0b8c4aac1ce1a9e31f168ae58030d0
automatylicza <automatylicza@gmail.com>
2024-12-19 14:57:52 +0100

    zmiana na titanic

--- titanic/plots.py
+++ titanic/plots.py
@@ -4,7 +4,7 @@ import typer
 from loguru import logger
 from tqdm import tqdm
 
-from bimba_2.config import FIGURES_DIR, PROCESSED_DATA_DIR
+from titanic.config import FIGURES_DIR, PROCESSED_DATA_DIR
 
 app = typer.Typer()
 
